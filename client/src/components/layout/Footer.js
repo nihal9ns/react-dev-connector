8d20fd5641d4bb6a0bee95fb735fe601437c82dc
Nihal <nihal9ns@gmail.com>
2019-04-22 10:20:49 +0530

    minor changes for footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,9 +3,18 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; {new Date().getFullYear()} Dev Connector
-      </footer>
+      <div className="footer">
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy; {new Date().getFullYear()} DevConnector
+          <br /> Made with{" "}
+          <img
+            className="imgClass"
+            src="http://www.clker.com/cliparts/D/H/K/c/a/j/red-heart-fire.svg"
+            alt="love"
+          />{" "}
+          by Nihal
+        </footer>
+      </div>
     );
   }
 }
